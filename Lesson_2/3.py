035be92b7e595209d493dde90692389068f07d3c
Farelkin <farelka.vanya@yandex.ru>
2019-05-22 17:34:50 +0300

    2 and 3

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -3,3 +3,17 @@
 цифр и вывести на экран. Например, если введено число 3486,
  то надо вывести число 6843.
 """
+
+num = int(input('введите число: '))
+result = ''
+while True:
+    res1 = num % 10
+    res2 = num // 10
+    if res2 == 0:
+        result += str(res1)
+        break
+    else:
+        num = res2
+        result += str(res1)
+
+print(result)
